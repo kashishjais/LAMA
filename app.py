d0fc69b1aed8786623ebd4ba95745eef1c08455f
Kashish Jaiswal <kashishjais23@gmail.com>
2022-05-12 19:27:41 +0530

    image loading solved

--- app.py
+++ app.py
@@ -1,13 +1,13 @@
-from flask import Flask, render_template, request, Response
+from flask import Flask, render_template, request, Response,redirect,session,flash
 from werkzeug.utils import secure_filename
+import os
+from db import MyUpload, db_init, db
 
-from db import db_init, db
-from models import Img
 import base64
 
 app = Flask(__name__)
-
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///img.db'
+app.secret_key='newproject'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///uploads.sqlite'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db_init(app)
 
@@ -18,35 +18,52 @@ def home():
 
 @app.route('/paint')
 def paint():
-    image=Img.query.all()
-    return render_template('index2.html')
+    image=MyUpload.query.filter_by(id=1).first()
+    return render_template('index2.html',data=image)
 
 @app.route('/remove')
 def remove():
     return render_template('tab3.html')
 
-@app.route('/upload', methods=['POST'])
+def allowed_files(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in {'jpg','png'}
+
+
+@app.route('/upload', methods=['POST','GET'])
 def upload():
-    pic = request.files['file']
-    if not pic:
-        return 'No pic uploaded!', 400
+    if request.method == 'POST':
+        print(request.files)
+        file= request.files['file']
+        if not file:
+            return 'No pic uploaded!', 400
 
-    filename = secure_filename(pic.filename)
-    mimetype = pic.mimetype
-    if not filename or not mimetype:
-        return 'Bad upload!', 400
+        filename = secure_filename(file.filename)
+        mimetype = file.mimetype
+        if not filename or not mimetype:
+            return 'Bad upload!', 400
+        if file and allowed_files(file.filename):
+                print(file.filename)
+                filename = secure_filename(file.filename)
+                file.save(os.path.join(os.getcwd(),"static/uploads",filename))
+                
+                upload = MyUpload(img =f"/static/uploads/{filename}", imgtype = os.path.splitext(file.filename)[1])
+                db.session.add(upload)
+                db.session.commit()
 
-    img = Img(img=pic.read(), name=filename, mimetype=mimetype)
-    db.session.add(img)
-    db.session.commit()
+                flash('file uploaded and saved','success')
+                session['uploaded_file'] = f"/static/uploads/{filename}"
+                return redirect(request.url)
+        else:
+            flash('wrong file selected, only PNG and JPG images allowed','danger')
+            return redirect(request.url)
 
-    return 'Img Uploaded!', 200
+  
+    return 'Img Uploaded!', 200    
     
-
-
+        
 @app.route('/<int:id>')
 def get_img(id):
-    img = Img.query.filter_by(id=id).first()
+    img = MyUpload.query.filter_by(id=id).first()
     if not img:
         return 'Img Not Found!', 404
 
--- db.py
+++ db.py
@@ -1,7 +1,16 @@
 from flask_sqlalchemy import SQLAlchemy
-
+from datetime import datetime
 db = SQLAlchemy()
 
+class MyUpload(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    img = db.Column(db.String(255))
+    imgtype = db.Column(db.String(4))
+   
+    created_on = db.Column(db.DateTime, index=True, default=datetime.now)
+
+    def __repr__(self):
+        return self.img
 
 # Function that initializes the db and creates the tables
 def db_init(app):
