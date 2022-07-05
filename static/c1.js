1db3e43d33a255ab06ed91c5b2a091c8f4587e5a
Kashish Jaiswal <kashishjais23@gmail.com>
2022-07-05 12:26:16 +0530

    .

--- static/c1.js
+++ static/c1.js
@@ -1,80 +1,70 @@
-
-
-window.onload = function () {
-  // Definitions
-  var canvas = document.getElementById("paint-canvas");
-  var context = canvas.getContext("2d");
-  var boundings = canvas.getBoundingClientRect();
-
-  // Specifications
-  var mouseX = 0;
-  var mouseY = 0;
-  context.strokeStyle = "black"; // initial brush color
-  context.lineWidth = 1; // initial brush width
-  var isDrawing = false;
-
-  // Handle Colors
-  var colors = document.getElementsByClassName("colors")[0];
-
-  colors.addEventListener("click", function (event) {
-    context.strokeStyle = event.target.value || "black";
-  });
-
-  // Handle Brushes
-  var brushes = document.getElementsByClassName("brushes")[0];
-
-  brushes.addEventListener("click", function (event) {
-    context.lineWidth = event.target.value || 1;
-  });
-
-  // Mouse Down Event
-  canvas.addEventListener("mousedown", function (event) {
-    setMouseCoordinates(event);
-    isDrawing = true;
-
-    // Start Drawing
-    context.beginPath();
-    context.moveTo(mouseX, mouseY);
-  });
-
-  // Mouse Move Event
-  canvas.addEventListener("mousemove", function (event) {
-    setMouseCoordinates(event);
-
-    if (isDrawing) {
-      context.lineTo(mouseX, mouseY);
-      context.stroke();
+window.onload = () => {
+    const canvas = document.getElementById('canvas');
+    const saveButton = document.getElementById('save');
+    const loadInput = document.getElementById('load');
+  
+    new Drawing(canvas, saveButton, loadInput);
+  };
+  
+  class Drawing {
+    constructor(canvas, saveButton, loadInput) {
+      this.isDrawing = false;
+  
+      canvas.addEventListener('mousedown', () => this.startDrawing());
+      canvas.addEventListener('mousemove', (event) => this.draw(event));
+      canvas.addEventListener('mouseup', () => this.stopDrawing());
+  
+      saveButton.addEventListener('click', () => this.save());
+      loadInput.addEventListener('change', (event) => this.load(event));
+  
+      const rect = canvas.getBoundingClientRect();
+  
+      this.offsetLeft = rect.left;
+      this.offsetTop = rect.top;
+  
+      this.canvas = canvas;
+      this.context = this.canvas.getContext('2d');
     }
-  });
-
-  // Mouse Up Event
-  canvas.addEventListener("mouseup", function (event) {
-    setMouseCoordinates(event);
-    isDrawing = false;
-  });
-
-  // Handle Mouse Coordinates
-  function setMouseCoordinates(event) {
-    mouseX = event.clientX - boundings.left;
-    mouseY = event.clientY - boundings.top;
+    startDrawing() {
+      this.isDrawing = true;
+    }
+    stopDrawing() {
+      this.isDrawing = false;
+    }
+    draw(event) {
+      if (this.isDrawing) {
+        this.context.fillRect(event.pageX - this.offsetLeft, event.pageY - this.offsetTop, 2, 2);
+      }
+    }
+    save() {
+      const data = this.canvas.toDataURL('image/png');
+      const a = document.createElement('a');
+      a.href = data;
+      a.download = 'image.png';
+      a.click();
+    }
+    load(event) {
+      const file = [...event.target.files].pop();
+      this.readTheFile(file)
+        .then((image) => this.loadTheImage(image))
+    }
+    loadTheImage(image) {
+      const img = new Image();
+      const canvas = this.canvas;
+      img.onload = function () {
+        const context = canvas.getContext('2d');
+        context.clearRect(0, 0, canvas.width, canvas.height);
+        context.drawImage(img, 0, 0);
+      };
+      img.src = image;
+    }
+    readTheFile(file) {
+      const reader = new FileReader();
+      return new Promise((resolve) => {
+        reader.onload = (event) => {
+          resolve(event.target.result);
+        };
+        reader.readAsDataURL(file);
+      })
+    }
   }
-
-  // Handle Clear Button
-  var clearButton = document.getElementById("clear");
-
-  clearButton.addEventListener("click", function () {
-    context.clearRect(0, 0, canvas.width, canvas.height);
-  });
-
-  // Handle Save Button
-  var saveButton = document.getElementById("save");
-
-  saveButton.addEventListener("click", function () {
-    var imageName = prompt("Please enter image name");
-    var canvasDataURL = canvas.toDataURL();
-    var a = document.createElement("a");
-    a.href = canvasDataURL;
-    a.download = imageName || "drawing";
-    a.click();
-  });
-};
